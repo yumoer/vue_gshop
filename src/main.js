2ab25034f532878398d52daacf06f06f5405c25b
yumo728 <794267701@qq.com>
2018-05-16 16:56:57 +0800

    first push

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/*入口js*/
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
